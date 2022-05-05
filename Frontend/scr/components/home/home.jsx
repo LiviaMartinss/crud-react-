9861f05085189f2cbe171fb5f2a7109bceb6c9cd
Lívia Martins <95534704+LiviaMartinss@users.noreply.github.com>
2022-05-05 09:04:12 -0300

    Create home.jsx

---
+++ Frontend/scr/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.css'
+import Main from '../template/main'
+
+export default props =>
+   <Main icon="Home" title="Início" subtitle="CRUD PROJECT">
+       <div className="display-4">Bem vindo</div>
+       <hr/>
+       <p className="mb-0">CRUD com React</p>
+   </Main>
