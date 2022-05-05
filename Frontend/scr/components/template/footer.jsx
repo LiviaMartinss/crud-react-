03d2c8365bb053a2f6aa0e101d66769b7f0f9a45
Lívia Martins <95534704+LiviaMartinss@users.noreply.github.com>
2022-05-05 09:05:17 -0300

    Create footer.jsx

---
+++ Frontend/scr/components/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.css'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong><span className="text-danger"> Lívia :) </span></strong>
+        </span>
+    </footer>
